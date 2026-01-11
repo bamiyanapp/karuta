124e0651ee5dd36ecb30e936c121a83d43a1ccb8
bamiyanapp <36719508+bamiyanapp@users.noreply.github.com>
2026-01-11 12:14:06 +0900

    fix: update .releaserc.cjs (#67)
    タグ条件を追加

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -2,7 +2,30 @@ module.exports = {
   branches: ["release"],
   repositoryUrl: "https://github.com/bamiyanapp/karuta.git",
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          // 小文字（正規）
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+
+          // 大文字対応
+          { type: "Feat", release: "minor" },
+          { type: "Fix", release: "patch" },
+
+          // スラッシュ系（今ある履歴対応）
+          { type: "Feat/fix", release: "patch" },
+          { type: "Fix/tech", release: "patch" },
+          { type: "Refactor", release: "patch" },
+
+          // 明示的にリリースしない
+          { type: "Docs", release: false },
+          { type: "Chore", release: false }
+        ]
+      }
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     [
